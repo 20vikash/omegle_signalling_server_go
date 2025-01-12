d42ba04586a96152906a5308a281c1c038b1194b
ssvikash_20 <arisivikash@gmail.com>
2025-01-12 15:06:56 +0000

    Manage multiple clients

--- cmd/ws.go
+++ cmd/ws.go
@@ -11,6 +11,8 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+var clients = make(map[*websocket.Conn]bool)
+
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 
@@ -21,6 +23,8 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 
 	defer ws.Close()
 
+	clients[ws] = true
+
 	for {
 		_, msg, err := ws.ReadMessage()
 		if err != nil {
@@ -29,9 +33,13 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		}
 		log.Printf("Received: %s\n", msg)
 
-		if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil {
-			log.Println("write error:", err)
-			break
+		for client := range clients {
+			if client != ws {
+				if err := client.WriteMessage(websocket.TextMessage, msg); err != nil {
+					log.Println("Write error:", err)
+					break
+				}
+			}
 		}
 	}
 }
