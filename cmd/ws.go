81c8d52790d835e40b7d9be34790b812c84dab35
ssvikash_20 <arisivikash@gmail.com>
2025-01-14 11:23:30 +0000

    Skip and find the next pair

--- cmd/ws.go
+++ cmd/ws.go
@@ -13,6 +13,7 @@ var upgrader = websocket.Upgrader{
 }
 
 var clients = make([]*websocket.Conn, 0)
+var pairs = make(map[*websocket.Conn]*websocket.Conn)
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
@@ -26,7 +27,10 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 
 	log.Println(clients)
 
-	con1, con2, err := match.Match_pair(&clients, Mu)
+	con1, con2, err := match.Match_pair(&clients, &pairs, Mu)
+	pairs[con1] = con2
+	pairs[con2] = con1
+
 	if err != nil {
 		log.Println("No pair found.. Waiting for another connection")
 	}
@@ -35,4 +39,46 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		con1.WriteMessage(websocket.TextMessage, []byte("You got a pair"))
 		con2.WriteMessage(websocket.TextMessage, []byte("You got a pair"))
 	}
+
+	for {
+		_, p, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+		}
+
+		message := string(p)
+
+		if message == "NEXT" {
+			on, tn, old, err := match.Next_pair(ws, &clients, &pairs, Mu)
+
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+
+			if tn == ws {
+				pairs[tn] = on
+				pairs[on] = tn
+
+				writeNewPair(ws, on)
+
+				continue
+			}
+
+			pairs[ws] = on
+			pairs[on] = ws
+			writeNewPair(ws, on)
+
+			if tn != nil {
+				pairs[old] = tn
+				pairs[tn] = old
+				writeNewPair(old, tn)
+			}
+		}
+	}
+}
+
+func writeNewPair(con1 *websocket.Conn, con2 *websocket.Conn) {
+	con1.WriteMessage(websocket.TextMessage, []byte("You got a new pair"))
+	con2.WriteMessage(websocket.TextMessage, []byte("You got a new pair"))
 }
--- internal/match/match.go
+++ internal/match/match.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func Match_pair(conns *[]*websocket.Conn, mu *sync.Mutex) (*websocket.Conn, *websocket.Conn, error) {
+func Match_pair(conns *[]*websocket.Conn, pairs *map[*websocket.Conn]*websocket.Conn, mu *sync.Mutex) (*websocket.Conn, *websocket.Conn, error) {
 	mu.Lock()
 
 	if len(*conns) < 2 {
@@ -27,6 +27,33 @@ func Match_pair(conns *[]*websocket.Conn, mu *sync.Mutex) (*websocket.Conn, *web
 	return con1, con2, nil
 }
 
-func Next_pair(conn *websocket.Conn) (*websocket.Conn, error) {
+func Next_pair(conn *websocket.Conn, conns *[]*websocket.Conn, pairs *map[*websocket.Conn]*websocket.Conn, mu *sync.Mutex) (*websocket.Conn, *websocket.Conn, *websocket.Conn, error) {
+	mu.Lock()
+	defer mu.Unlock()
 
+	conn2, exists := (*pairs)[conn]
+
+	if !exists {
+		return nil, nil, conn2, errors.New("no connections found yet")
+	}
+
+	delete((*pairs), conn)
+	delete((*pairs), conn2)
+
+	if len(*conns) == 0 {
+		return conn2, conn, conn2, nil
+	}
+
+	con1 := (*conns)[rand.Intn(len(*conns))]
+	*conns = helper.RemoveSliceElement(*conns, con1)
+
+	if len(*conns) == 0 {
+		*conns = append(*conns, conn2)
+		return con1, nil, conn2, nil
+	}
+
+	con2 := (*conns)[rand.Intn(len(*conns))]
+	*conns = helper.RemoveSliceElement(*conns, con2)
+
+	return con1, con2, conn2, nil
 }
